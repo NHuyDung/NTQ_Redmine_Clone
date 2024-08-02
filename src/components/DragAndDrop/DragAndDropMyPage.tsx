9c747262fae5df7eb8fc7766ffa6b94081b17803
ducnmhe150286 <97494895+ducnmhe150286@users.noreply.github.com>
2024-08-02 17:04:59 +0700

    fix: dialog issue

--- src/components/DragAndDrop/DragAndDropMyPage.tsx
+++ src/components/DragAndDrop/DragAndDropMyPage.tsx
@@ -32,21 +32,12 @@ const componentsMap = {
 };
 
 const DragAndDrop: React.FC<DragAndDropProps> = ({ items, hasBorder }) => {
-  const getLabelById = (itemId: string, targetList: "A" | "B" | "C") => {
-    const storedItems = JSON.parse(localStorage.getItem("items") || "{}");
-    const item = storedItems[targetList].find((item: { id: string }) => item.id === itemId);
-    return item ? item.label : "Unknown label";
-  };
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const renderItems = (items: Item[] | [], targetList: "A" | "B" | "C") => {
     return items.map((item) => {
       const Component = componentsMap[item.componentName as keyof typeof componentsMap];
       return (
         <div key={item.id} className="item">
-          <div className="flex flex-1 mb-2">
-            <a className="text-primary">{getLabelById(item.id, targetList)}</a>
-          </div>
           {Component ? <Component id={item.id} /> : null}
         </div>
       );
